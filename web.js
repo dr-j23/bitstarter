1afafbca2e55f512713ab4c34ae52541b3504b5a
JP <JP@JP.com>
2013-08-15 13:20:14 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -3,7 +3,11 @@ var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  var buffer = fs.readFileSync('index.html',function(error,data){
+      if (error) throw error;
+      console.log(data);
+      });
+  response.send(buffer.toString("utf-8"));
 });
 
 var port = process.env.PORT || 5000;
